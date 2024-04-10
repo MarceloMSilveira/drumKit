4739c622170857545bb197b83082fdc496a28716
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2024-04-10 10:31:25 -0300

    play sound v1

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ function wasClicked() {
     alert("I got a click!");
 }
 
-var buttons = document.querySelectorAll("button");
+function playSound(){
+    var sound1 = new Audio("./sounds/tom-1.mp3");
+    sound1.play();
+}
+
+var buttons = document.querySelectorAll(".drum");
 var arrayLength = buttons.length;
 for (var i=0; i<arrayLength; i++){
-    buttons[i].addEventListener("click",function (){alert("I got a click!")})
+    buttons[i].addEventListener("click", playSound)
 }
