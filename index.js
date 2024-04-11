b4d098c7856e0fa8781e88793be2174c615ba9d1
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2024-04-10 21:20:39 -0300

    play sounds with keyboard

--- index.js
+++ index.js
@@ -7,41 +7,48 @@ function playSound(caminhoToSound){
     return sound1.play();
 }
 
-var buttons = document.querySelectorAll(".drum");
-var arrayLength = buttons.length;
-
-for (var i=0; i<arrayLength; i++){
-    buttons[i].addEventListener("click", function(){
-        switch (this.innerText) {
-            case "w":
-                playSound("./sounds/tom-1.mp3");
-                break;
-            case "a":
-                playSound("./sounds/tom-2.mp3");
-                break;
-            case "s":
-                playSound("./sounds/tom-3.mp3");
-                break;
-            case "d":
-                playSound("./sounds/tom-4.mp3");
-                break;
-            case "j":
-                playSound("./sounds/snare.mp3");
-                break;
-            case "k":
-                playSound("./sounds/crash.mp3");
-                break;
-            case "l":
-                playSound("./sounds/kick-bass.mp3");
-                break;
-            default:
-                console.log("no sound")
-            
-        }
-    })
+function selectSoundFile(caracter) {
+    switch (caracter) {
+        case "w":
+            //this.style.color="white";
+            playSound("./sounds/tom-1.mp3");
+            break;
+        case "a":
+            playSound("./sounds/tom-2.mp3");
+            break;
+        case "s":
+            playSound("./sounds/tom-3.mp3");
+            break;
+        case "d":
+            playSound("./sounds/tom-4.mp3");
+            break;
+        case "j":
+            playSound("./sounds/snare.mp3");
+            break;
+        case "k":
+            playSound("./sounds/crash.mp3");
+            break;
+        case "l":
+            playSound("./sounds/kick-bass.mp3");
+            break;
+        default:
+            console.log("no sound")
+        
+    }
 }
 
+//checking a bottom press
+var buttons = document.querySelectorAll(".drum");
+var arrayLength = buttons.length;
+for (var i=0; i<arrayLength; i++){
+    buttons[i].addEventListener("click", function () {
+        selectSoundFile(this.textContent)});
+}
 
+//checking key press
+document.addEventListener("keydown",function(e){
+    selectSoundFile(e.key.toLowerCase());
+})
 /*for (var i=0; i<arrayLength; i++){
     switch (buttons[i].innerText) {
         case "w":
